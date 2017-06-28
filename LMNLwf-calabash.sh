72ee29d57459e4ddd538f536df47e6bb792912ff
djbpitt <djbpitt@pitt.edu>
2017-06-28 18:23:26 -0400

    resolving merge conflicts

--- LMNLwf-calabash.sh
+++ LMNLwf-calabash.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 FILENAME=$1
-<<<<<<< HEAD
 CALABASH=/Applications/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar
 PIPELINE=file:///Users/djb/Luminescent/LMNL-wf-check.xpl
-=======
-CALABASH=/home/wendell/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar
-PIPELINE=file:///home/wendell/Documents/Luminescent/LMNL-wf-check.xpl
->>>>>>> upstream/master
 
 echo LMNL syntax checking ... $FILENAME ...
 java -Xmx1024m -jar $CALABASH -dtext/plain@$FILENAME $PIPELINE
--- Luminescent-calabash.sh
+++ Luminescent-calabash.sh
@@ -2,11 +2,7 @@
 
 # For example, >Luminescent-calabash LMNL-process-example lmnl/frost-quote.lmnl
  
-<<<<<<< HEAD
 CALABASH=/Applications/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar
-=======
-CALABASH=/home/wendell/xmlcalabash-1.1.16-97/xmlcalabash-1.1.16-97.jar
->>>>>>> upstream/master
 PIPELINE=$1.xpl
 FILENAME=$2
 
